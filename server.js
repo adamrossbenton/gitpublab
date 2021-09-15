327de18ce9ae6facb737d4b47a06b892697ab364
Adam Ross Benton <adamrossbenton@gmail.com>
2021-09-14 18:04:16 -0700

    res.send(drinks) set up

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+const drinks = require("./models/drinks")
 
+app.get("/drinks",(req,res) => {
+    res.send(drinks)
+})
 
 app.listen(port, () => {
     console.log(`Welcome to the Gitpub App!`)
