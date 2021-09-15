6cad6d1580735fa81539e3bcf1d4960ba29416b3
Adam Ross Benton <adamrossbenton@gmail.com>
2021-09-14 18:49:06 -0700

    food paths

--- server.js
+++ server.js
@@ -6,11 +6,27 @@ const food = require("./models/food")
 const drinks = require("./models/drinks")
 
 // index
+app.get('/',(req,res) => {
+    res.send(`
+        <h1>Welcome to gitPub!</h1>
+        <a href="./food">FOOD</a><br />
+        <a href="./drinks">DRINKS</a>
+    `)
+})
+
+// show
+app.get("/food",(req,res) => {
+    res.render("foodindex.ejs", {allFood: food})
+})
+
+app.get("/drinks/:id",(req,res) => {
+    res.render("foodshow.ejs",{fooditem: food[req.params.id]})
+})
+
 app.get("/drinks",(req,res) => {
     res.render("drinksindex.ejs", {allDrinks: drinks})
 })
 
-// show
 app.get("/drinks/:id",(req,res) => {
     res.render("drinksshow.ejs", {drink: drinks[req.params.id]})
 })
